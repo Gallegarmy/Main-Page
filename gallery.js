5141fe29db4ec9a59570161eaf46860243d3c9d9
Qrow <nachoesposito@gmail.com>
2024-08-11 16:02:45 +0200

    Gallery close-up added
    Former-commit-id: 8d4d3d992f14fb1471974a59c59d3bdcab2e0468 [formerly f870eacfd5a09b2a327108b52a0449cd333e6db8]
    Former-commit-id: c521272fbabc9a5c9e6bd116b0d7c238b39c1697

--- gallery.js
+++ gallery.js
@@ -1,4 +1,34 @@
 document.addEventListener("DOMContentLoaded", function() {
+    var modal = document.getElementById('imageModal');
+
+    // Get the image and insert it inside the modal
+    var modalImg = document.getElementById('modalImage');
+    var captionText = document.getElementById('caption');
+    const regex = /\.\/img\/[a-zA-Z0-9_\-]+\.jpg/g;
+    var items = document.querySelectorAll('.gallery-item');
+    console.log(items);
+
+    items.forEach(function(item) {
+        item.onclick = function() {
+            var imgsrc = item.outerHTML.match(regex)[0];
+            modal.style.display = "flex";
+            modalImg.src = imgsrc;
+        };
+    });
+
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    };
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
     let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
 
     if ("IntersectionObserver" in window) {
