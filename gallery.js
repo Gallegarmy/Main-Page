813f635efad957891fb55988fc37f9314bc8521b
Néstor <nestor@nstlopez.com>
2024-08-12 17:07:22 +0200

    feat: cleanup js code of imperfections
    Former-commit-id: 33d144bf8af92da5620a871bcc8bc27698d2e3ba [formerly ea3b3b66a9f2033d2dd4191365b483f0030a8089]
    Former-commit-id: 3f51a1c20ef2c6829ea2c2b3f88cfe2a62222174

--- gallery.js
+++ gallery.js
@@ -1,58 +1,53 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var modal = document.getElementById("imageModal");
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.getElementById("imageModal");
 
   // Get the image and insert it inside the modal
-  var modalImg = document.getElementById("modalImage");
-  var captionText = document.getElementById("caption");
-  const regex = /\.\/img\/[a-zA-Z0-9_\-]+\.jpg/g;
-  var items = document.querySelectorAll(".gallery-item");
-  console.log(items);
+  const modalImg = document.getElementById("modalImage");
+  const regex = /\.\/img\/[a-zA-Z0-9_\-]+\.webp/g;
+  const items = document.querySelectorAll(".gallery-item");
 
-  items.forEach(function (item) {
-    item.onclick = function () {
-      var imgsrc = item.outerHTML.match(regex)[0];
+  for (const item of items) {
+    item.onclick = () => {
+      const imgsrc = item.outerHTML.match(regex)[0];
       modal.style.display = "flex";
       modalImg.src = imgsrc;
     };
-  });
-
-  var span = document.getElementsByClassName("close")[0];
-
-  span.onclick = function () {
-    modal.style.display = "none";
-  };
-
-  window.onclick = function (event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  };
-  let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
-
-  if ("IntersectionObserver" in window) {
-    let lazyImageObserver = new IntersectionObserver(function (
-      entries,
-      observer
-    ) {
-      entries.forEach(function (entry) {
-        if (entry.isIntersecting) {
-          let lazyImage = entry.target;
-          lazyImage.src = lazyImage.dataset.src;
-          lazyImage.onload = () => lazyImage.classList.add("loaded");
-          lazyImage.classList.remove("lazy");
-          lazyImageObserver.unobserve(lazyImage);
-        }
-      });
-    });
-
-    lazyImages.forEach(function (lazyImage) {
-      lazyImageObserver.observe(lazyImage);
-    });
-  } else {
-    lazyImages.forEach(function (lazyImage) {
-      lazyImage.src = lazyImage.dataset.src;
-      lazyImage.onload = () => lazyImage.classList.add("loaded");
-      lazyImage.classList.remove("lazy");
-    });
   }
 });
+
+const span = document.getElementsByClassName("close")[0];
+
+span.onclick = () => {
+  modal.style.display = "none";
+};
+
+window.onclick = (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+};
+const lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+
+if ("IntersectionObserver" in window) {
+  const lazyImageObserver = new IntersectionObserver((entries, observer) => {
+    for (const entry of entries) {
+      if (entry.isIntersecting) {
+        const lazyImage = entry.target;
+        lazyImage.src = lazyImage.dataset.src;
+        lazyImage.onload = () => lazyImage.classList.add("loaded");
+        lazyImage.classList.remove("lazy");
+        lazyImageObserver.unobserve(lazyImage);
+      }
+    }
+  });
+
+  for (const lazyImage of lazyImages) {
+    lazyImageObserver.observe(lazyImage);
+  }
+} else {
+  for (const lazyImage of lazyImages) {
+    lazyImage.src = lazyImage.dataset.src;
+    lazyImage.onload = () => lazyImage.classList.add("loaded");
+    lazyImage.classList.remove("lazy");
+  }
+}
--- language.js
+++ language.js
@@ -1,65 +1,67 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var savedLanguage = localStorage.getItem("language");
+document.addEventListener("DOMContentLoaded", () => {
+  const savedLanguage = localStorage.getItem("language");
   if (savedLanguage) {
-    if (savedLanguage == "gl") {
-      var elementosEsp = document.querySelectorAll(".Esp"); // Seleccionar todos los elementos con la clase 'miObjeto'
-      elementosEsp.forEach(function (elemento) {
+    if (savedLanguage === "gl") {
+      const elementosEsp = document.querySelectorAll(".Esp"); // Seleccionar todos los elementos con la clase 'miObjeto'
+      for (const elemento of elementosEsp) {
         elemento.style.display = "none";
-      });
-      var elementosGal = document.querySelectorAll(".Gal"); // Seleccionar todos los elementos con la clase 'miObjeto'
-      elementosGal.forEach(function (elemento) {
+      }
+      const elementosGal = document.querySelectorAll(".Gal"); // Seleccionar todos los elementos con la clase 'miObjeto'
+      for (const elemento of elementosGal) {
         elemento.style.display = "";
-      });
+      }
       document.getElementById("EspButton").innerText = "Castelán";
       document.getElementById("GalButton").innerText = "Galego";
-    } else if (savedLanguage == "es") {
-      var elementosEsp = document.querySelectorAll(".Esp"); // Seleccionar todos los elementos con la clase 'miObjeto'
-      elementosEsp.forEach(function (elemento) {
+    } else if (savedLanguage === "es") {
+      const elementosEsp = document.querySelectorAll(".Esp"); // Seleccionar todos los elementos con la clase 'miObjeto'
+      for (const elemento of elementosEsp) {
         elemento.style.display = "";
-      });
-      var elementosGal = document.querySelectorAll(".Gal"); // Seleccionar todos los elementos con la clase 'miObjeto'
-      elementosGal.forEach(function (elemento) {
+      }
+      const elementosGal = document.querySelectorAll(".Gal"); // Seleccionar todos los elementos con la clase 'miObjeto'
+      for (const elemento of elementosGal) {
         elemento.style.display = "none";
-      });
+      }
       document.getElementById("EspButton").innerText = "Español";
       document.getElementById("GalButton").innerText = "Gallego";
     }
   } else {
-    var elementosEsp = document.querySelectorAll(".Esp"); // Seleccionar todos los elementos con la clase 'miObjeto'
-    elementosEsp.forEach(function (elemento) {
+    const elementosEsp = document.querySelectorAll(".Esp"); // Seleccionar todos los elementos con la clase 'miObjeto'
+    for (const elemento of elementosEsp) {
       elemento.style.display = "";
-    });
-    var elementosGal = document.querySelectorAll(".Gal"); // Seleccionar todos los elementos con la clase 'miObjeto'
-    elementosGal.forEach(function (elemento) {
+    }
+    const elementosGal = document.querySelectorAll(".Gal"); // Seleccionar todos los elementos con la clase 'miObjeto'
+    for (const elemento of elementosGal) {
       elemento.style.display = "none";
-    });
+    }
     document.getElementById("EspButton").innerText = "Español";
     document.getElementById("GalButton").innerText = "Gallego";
   }
 });
 
 function changeLanguage(idioma) {
-  if (idioma == "gl") {
-    var elementosEsp = document.querySelectorAll(".Esp"); // Seleccionar todos los elementos con la clase 'miObjeto'
-    elementosEsp.forEach(function (elemento) {
+  if (idioma === "gl") {
+    const elementosEsp = document.querySelectorAll(".Esp"); // Seleccionar todos los elementos con la clase 'miObjeto'
+    for (const elemento of elementosEsp) {
       elemento.style.display = "none";
-    });
-    var elementosGal = document.querySelectorAll(".Gal"); // Seleccionar todos los elementos con la clase 'miObjeto'
-    elementosGal.forEach(function (elemento) {
+    }
+    const elementosGal = document.querySelectorAll(".Gal"); // Seleccionar todos los elementos con la clase 'miObjeto'
+    for (const elemento of elementosGal) {
       elemento.style.display = "";
-    });
+    }
+
     localStorage.setItem("language", "gl");
     document.getElementById("EspButton").innerText = "Castelán";
     document.getElementById("GalButton").innerText = "Galego";
-  } else if (idioma == "es") {
-    var elementosEsp = document.querySelectorAll(".Esp"); // Seleccionar todos los elementos con la clase 'miObjeto'
-    elementosEsp.forEach(function (elemento) {
+  } else if (idioma === "es") {
+    const elementosEsp = document.querySelectorAll(".Esp"); // Seleccionar todos los elementos con la clase 'miObjeto'
+    for (const elemento of elementosEsp) {
       elemento.style.display = "";
-    });
-    var elementosGal = document.querySelectorAll(".Gal"); // Seleccionar todos los elementos con la clase 'miObjeto'
-    elementosGal.forEach(function (elemento) {
+    }
+    const elementosGal = document.querySelectorAll(".Gal"); // Seleccionar todos los elementos con la clase 'miObjeto'
+    for (const elemento of elementosGal) {
       elemento.style.display = "none";
-    });
+    }
+
     localStorage.setItem("language", "es");
     document.getElementById("EspButton").innerText = "Español";
     document.getElementById("GalButton").innerText = "Gallego";
