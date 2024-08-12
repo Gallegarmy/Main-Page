4b3b5ad5d881f5235be985c74a694b47c69970e2
Néstor <nestor@nstlopez.com>
2024-08-12 17:01:36 +0200

    chore: format all files
    Former-commit-id: 5e86f0405cc48a14ab4138023b5bd9d7d0e91467 [formerly 4ec7f38c1ef1925975ea9057607cbebbe0fe896e]
    Former-commit-id: ea74b245db4f9c67cb38a4f6171b3ed657830804

--- gallery.js
+++ gallery.js
@@ -1,55 +1,58 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var modal = document.getElementById('imageModal');
+document.addEventListener("DOMContentLoaded", function () {
+  var modal = document.getElementById("imageModal");
 
-    // Get the image and insert it inside the modal
-    var modalImg = document.getElementById('modalImage');
-    var captionText = document.getElementById('caption');
-    const regex = /\.\/img\/[a-zA-Z0-9_\-]+\.jpg/g;
-    var items = document.querySelectorAll('.gallery-item');
-    console.log(items);
+  // Get the image and insert it inside the modal
+  var modalImg = document.getElementById("modalImage");
+  var captionText = document.getElementById("caption");
+  const regex = /\.\/img\/[a-zA-Z0-9_\-]+\.jpg/g;
+  var items = document.querySelectorAll(".gallery-item");
+  console.log(items);
 
-    items.forEach(function(item) {
-        item.onclick = function() {
-            var imgsrc = item.outerHTML.match(regex)[0];
-            modal.style.display = "flex";
-            modalImg.src = imgsrc;
-        };
+  items.forEach(function (item) {
+    item.onclick = function () {
+      var imgsrc = item.outerHTML.match(regex)[0];
+      modal.style.display = "flex";
+      modalImg.src = imgsrc;
+    };
+  });
+
+  var span = document.getElementsByClassName("close")[0];
+
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+  let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+
+  if ("IntersectionObserver" in window) {
+    let lazyImageObserver = new IntersectionObserver(function (
+      entries,
+      observer
+    ) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          let lazyImage = entry.target;
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.onload = () => lazyImage.classList.add("loaded");
+          lazyImage.classList.remove("lazy");
+          lazyImageObserver.unobserve(lazyImage);
+        }
+      });
     });
 
-    var span = document.getElementsByClassName("close")[0];
-
-    span.onclick = function() {
-        modal.style.display = "none";
-    };
-
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    };
-    let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
-
-    if ("IntersectionObserver" in window) {
-        let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
-            entries.forEach(function(entry) {
-                if (entry.isIntersecting) {
-                    let lazyImage = entry.target;
-                    lazyImage.src = lazyImage.dataset.src;
-                    lazyImage.onload = () => lazyImage.classList.add("loaded");
-                    lazyImage.classList.remove("lazy");
-                    lazyImageObserver.unobserve(lazyImage);
-                }
-            });
-        });
-
-        lazyImages.forEach(function(lazyImage) {
-            lazyImageObserver.observe(lazyImage);
-        });
-    } else {
-        lazyImages.forEach(function(lazyImage) {
-            lazyImage.src = lazyImage.dataset.src;
-            lazyImage.onload = () => lazyImage.classList.add("loaded");
-            lazyImage.classList.remove("lazy");
-        });
-    }
+    lazyImages.forEach(function (lazyImage) {
+      lazyImageObserver.observe(lazyImage);
+    });
+  } else {
+    lazyImages.forEach(function (lazyImage) {
+      lazyImage.src = lazyImage.dataset.src;
+      lazyImage.onload = () => lazyImage.classList.add("loaded");
+      lazyImage.classList.remove("lazy");
+    });
+  }
 });
--- language.js
+++ language.js
@@ -1,68 +1,67 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var savedLanguage = localStorage.getItem('language');
-    if (savedLanguage) {
-        if (savedLanguage == 'gl') {
-            var elementosEsp = document.querySelectorAll('.Esp'); // Seleccionar todos los elementos con la clase 'miObjeto'
-            elementosEsp.forEach(function (elemento) {
-                elemento.style.display = 'none';
-            });
-            var elementosGal = document.querySelectorAll('.Gal'); // Seleccionar todos los elementos con la clase 'miObjeto'
-            elementosGal.forEach(function (elemento) {
-                elemento.style.display = '';
-            });
-            document.getElementById("EspButton").innerText = "Castelán";
-            document.getElementById("GalButton").innerText = "Galego";
-        } else if (savedLanguage == 'es') {
-            var elementosEsp = document.querySelectorAll('.Esp'); // Seleccionar todos los elementos con la clase 'miObjeto'
-            elementosEsp.forEach(function (elemento) {
-                elemento.style.display = '';
-            });
-            var elementosGal = document.querySelectorAll('.Gal'); // Seleccionar todos los elementos con la clase 'miObjeto'
-            elementosGal.forEach(function (elemento) {
-                elemento.style.display = 'none';
-            });
-            document.getElementById("EspButton").innerText = "Español";
-            document.getElementById("GalButton").innerText = "Gallego";
-        }
-    } else {
-        var elementosEsp = document.querySelectorAll('.Esp'); // Seleccionar todos los elementos con la clase 'miObjeto'
-        elementosEsp.forEach(function (elemento) {
-            elemento.style.display = '';
-        });
-        var elementosGal = document.querySelectorAll('.Gal'); // Seleccionar todos los elementos con la clase 'miObjeto'
-        elementosGal.forEach(function (elemento) {
-            elemento.style.display = 'none';
-        });
-        document.getElementById("EspButton").innerText = "Español";
-        document.getElementById("GalButton").innerText = "Gallego";
+  var savedLanguage = localStorage.getItem("language");
+  if (savedLanguage) {
+    if (savedLanguage == "gl") {
+      var elementosEsp = document.querySelectorAll(".Esp"); // Seleccionar todos los elementos con la clase 'miObjeto'
+      elementosEsp.forEach(function (elemento) {
+        elemento.style.display = "none";
+      });
+      var elementosGal = document.querySelectorAll(".Gal"); // Seleccionar todos los elementos con la clase 'miObjeto'
+      elementosGal.forEach(function (elemento) {
+        elemento.style.display = "";
+      });
+      document.getElementById("EspButton").innerText = "Castelán";
+      document.getElementById("GalButton").innerText = "Galego";
+    } else if (savedLanguage == "es") {
+      var elementosEsp = document.querySelectorAll(".Esp"); // Seleccionar todos los elementos con la clase 'miObjeto'
+      elementosEsp.forEach(function (elemento) {
+        elemento.style.display = "";
+      });
+      var elementosGal = document.querySelectorAll(".Gal"); // Seleccionar todos los elementos con la clase 'miObjeto'
+      elementosGal.forEach(function (elemento) {
+        elemento.style.display = "none";
+      });
+      document.getElementById("EspButton").innerText = "Español";
+      document.getElementById("GalButton").innerText = "Gallego";
     }
+  } else {
+    var elementosEsp = document.querySelectorAll(".Esp"); // Seleccionar todos los elementos con la clase 'miObjeto'
+    elementosEsp.forEach(function (elemento) {
+      elemento.style.display = "";
+    });
+    var elementosGal = document.querySelectorAll(".Gal"); // Seleccionar todos los elementos con la clase 'miObjeto'
+    elementosGal.forEach(function (elemento) {
+      elemento.style.display = "none";
+    });
+    document.getElementById("EspButton").innerText = "Español";
+    document.getElementById("GalButton").innerText = "Gallego";
+  }
 });
 
-
 function changeLanguage(idioma) {
-    if (idioma == 'gl') {
-        var elementosEsp = document.querySelectorAll('.Esp'); // Seleccionar todos los elementos con la clase 'miObjeto'
-        elementosEsp.forEach(function (elemento) {
-            elemento.style.display = 'none';
-        });
-        var elementosGal = document.querySelectorAll('.Gal'); // Seleccionar todos los elementos con la clase 'miObjeto'
-        elementosGal.forEach(function (elemento) {
-            elemento.style.display = '';
-        });
-        localStorage.setItem('language', 'gl');
-        document.getElementById("EspButton").innerText = "Castelán";
-        document.getElementById("GalButton").innerText = "Galego";
-    } else if (idioma == 'es') {
-        var elementosEsp = document.querySelectorAll('.Esp'); // Seleccionar todos los elementos con la clase 'miObjeto'
-        elementosEsp.forEach(function (elemento) {
-            elemento.style.display = '';
-        });
-        var elementosGal = document.querySelectorAll('.Gal'); // Seleccionar todos los elementos con la clase 'miObjeto'
-        elementosGal.forEach(function (elemento) {
-            elemento.style.display = 'none';
-        });
-        localStorage.setItem('language', 'es');
-        document.getElementById("EspButton").innerText = "Español";
-        document.getElementById("GalButton").innerText = "Gallego";
-    }
+  if (idioma == "gl") {
+    var elementosEsp = document.querySelectorAll(".Esp"); // Seleccionar todos los elementos con la clase 'miObjeto'
+    elementosEsp.forEach(function (elemento) {
+      elemento.style.display = "none";
+    });
+    var elementosGal = document.querySelectorAll(".Gal"); // Seleccionar todos los elementos con la clase 'miObjeto'
+    elementosGal.forEach(function (elemento) {
+      elemento.style.display = "";
+    });
+    localStorage.setItem("language", "gl");
+    document.getElementById("EspButton").innerText = "Castelán";
+    document.getElementById("GalButton").innerText = "Galego";
+  } else if (idioma == "es") {
+    var elementosEsp = document.querySelectorAll(".Esp"); // Seleccionar todos los elementos con la clase 'miObjeto'
+    elementosEsp.forEach(function (elemento) {
+      elemento.style.display = "";
+    });
+    var elementosGal = document.querySelectorAll(".Gal"); // Seleccionar todos los elementos con la clase 'miObjeto'
+    elementosGal.forEach(function (elemento) {
+      elemento.style.display = "none";
+    });
+    localStorage.setItem("language", "es");
+    document.getElementById("EspButton").innerText = "Español";
+    document.getElementById("GalButton").innerText = "Gallego";
+  }
 }
