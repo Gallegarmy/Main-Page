c1ec43fa38ccf7c5da2a831ac66602b4d07d0d5a
Alessandra Amicarella <17497470+Alessag@users.noreply.github.com>
2025-08-20 01:09:07 +0200

    chore(data): add mocked community resources

---
+++ src/app/CommunityResources/data.ts
@@ -0,0 +1,32 @@
+import { CommunityResourcesInterface } from '@/types/CommunityResources';
+
+export const communityResourcesData: CommunityResourcesInterface[] = [
+  {
+    id: '1',
+    icon: 'string',
+    title: '#adminCanas',
+    description: 'Mollit aute non cillum quis ullamco.',
+    link: '/',
+  },
+  {
+    id: '2',
+    icon: 'string',
+    title: 'Github',
+    description: 'Mollit aute non cillum quis ullamco.',
+    link: '/',
+  },
+  {
+    id: '3',
+    icon: 'string',
+    title: 'Talleres',
+    description: 'Mollit aute non cillum quis ullamco.',
+    link: '/',
+  },
+  {
+    id: '4',
+    icon: 'string',
+    title: 'Timeline',
+    description: 'Mollit aute non cillum quis ullamco.',
+    link: '/',
+  },
+];
