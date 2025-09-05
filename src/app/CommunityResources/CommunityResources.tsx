e6bb7017330d60276ecdd18cac206f7e69d3e473
Alessandra Amicarella <17497470+Alessag@users.noreply.github.com>
2025-09-06 00:35:49 +0200

    feat(community): add community social links and improve component

--- src/app/CommunityResources/CommunityResources.tsx
+++ src/app/CommunityResources/CommunityResources.tsx
@@ -1,21 +1,35 @@
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
+import { communityIconMap } from './Icons';
 import { communityResourcesData } from './data';
 
 export const CommunityResources = () => {
   return (
-    <div className="border-2 border-green-300 w-full">
-      <ul className="border-2 border-white grid grid-cols-1 md:grid-cols-2 gap-6 my-9">
-        {communityResourcesData.map((element) => (
-          <li
-            key={element.id}
-            className="border border-white w-full md:w-min-[360px] md:max-w-[600px] h-[250px] flex flex-col gap-5 p-8"
-          >
-            <div className="border-2 border-white w-12 h-12 ">Icon</div>
-            <h5 className="border-2 border-white text-2xl font-bold">{element.title}</h5>
-            <p className="border-2 border-white text-base">{element.description}</p>
-          </li>
-        ))}
+    <div className="flex justify-center w-full px-4">
+      <ul className="grid grid-cols-1 justify-items-center lg:max-w-[1200px] md:grid-cols-2 gap-4 md:gap-6 my-24">
+        {communityResourcesData.map((element) => {
+          const icon: IconProp | undefined = communityIconMap[element.icon];
+          const isExternalLink = element.link.startsWith('http');
+
+          return (
+            <li key={element.id}>
+              <a
+                href={element.link}
+                target={isExternalLink ? '_blank' : '_self'}
+                rel={isExternalLink ? 'noopener noreferrer' : undefined}
+                className="border border-white w-full lg:min-w-full lg:w-[550px] min-h-[180px] sm:min-h-[200px] flex flex-col p-4 sm:p-6 md:p-8 cursor-pointer "
+              >
+                <div className="flex-shrink-0 w-9 h-9  md:w-10 md:h-10 flex items-center justify-center mb-4">
+                  {icon && <FontAwesomeIcon icon={icon} className="w-full h-full text-white" />}
+                </div>
+                <h5 className="text-xl sm:text-2xl font-bold mb-2">{element.title}</h5>
+                <p className="text-sm sm:text-base leading-relaxed">{element.description}</p>
+              </a>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
