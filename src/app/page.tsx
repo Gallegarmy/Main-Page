8f5ca69df9c2a69a3f4cd8de0471c4b484082d6d
Alessandra Amicarella <17497470+Alessag@users.noreply.github.com>
2025-09-06 01:44:29 +0200

    chore(image): Update Sysarmy logo path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { heroSliderData } from './data/heroSliderData';
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <HeroSlider slides={heroSliderData} logo="/assets/sysarmy-galicia-logo.png" />
+      <HeroSlider slides={heroSliderData} logo="/assets/communities/sysarmy-galicia/sysarmy-galicia-logo.png" />
       <CommunityResources />
       <FriendCommunities />
     </div>
