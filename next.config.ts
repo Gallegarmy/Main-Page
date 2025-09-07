7440e11a469bd9480b56736f8a087cd1d8cb4a22
Alessandra Amicarella <17497470+Alessag@users.noreply.github.com>
2025-09-07 12:48:32 +0200

    chore(image): Prepare next config to fetch images from the sysarmy cloud service

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cloud.sysarmy.galicia.dev',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
