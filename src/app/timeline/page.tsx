5b6c17637e570e208f8f681b7fb4cf710be7b306
Alessandra Amicarella <17497470+Alessag@users.noreply.github.com>
2025-11-13 15:15:55 +0100

    feat(header): add header to the timeline page

--- src/app/timeline/page.tsx
+++ src/app/timeline/page.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
+import { Header } from '../Header';
 import { TimelineGrid } from '../TimelineGrid';
 
 export default function Timeline() {
   return (
-    <div className="container mx-auto my-0">
-      <h1 className="my-8 text-center text-2xl font-bold lg:text-4xl xl:text-6xl">Timeline</h1>
-      <TimelineGrid />
-    </div>
+    <>
+      <Header />
+      <div className="container mx-auto my-0">
+        <h1 className="my-8 text-center text-2xl font-bold lg:text-4xl xl:text-5xl">Timeline</h1>
+        <TimelineGrid />
+      </div>
+    </>
   );
 }
