79a6d671cbbc930e58ac978d69e6181593407856
Alessandra Amicarella <17497470+Alessag@users.noreply.github.com>
2026-03-28 01:04:35 +0100

    feat(merch): add merge data and types

---
+++ src/data/MerchData.ts
@@ -0,0 +1,12 @@
+import { Merch } from '@/types/Merch';
+
+export const merchData: Merch[] = [
+  {
+    id: '1',
+    name: 'T-shirt',
+    description: 'Sysarmy Galicia Edition',
+    image: '/assets/merch/sysarmy-tshirt.jpg',
+    link: 'https://buy.stripe.com/14AeV520vgXo8IwaIlbAs00',
+    price: '30€',
+  },
+];
